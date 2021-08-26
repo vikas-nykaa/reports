4c9c56f1d31f71349ad0b1068f0aabe73e5b41a0
Vikas <vikas@ip-10-10-20-2.ap-south-1.compute.internal>
2021-08-26 14:45:40 +0530

    Add setup

---
+++ src/main/java/com/vikas/reports/VikasApplication.java
@@ -0,0 +1,13 @@
+package com.vikas.reports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VikasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VikasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vikas/reports/VikasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikas.reports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VikasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
